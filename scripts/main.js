b079f96a0064f24c62455e3a8bdbcb8faf0a097e
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-02-17 12:10:09 -0400

    Added duplicate filtering to search results and location list

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,47 @@
 import { getCoords, getWeather } from "./fetchers.js";
 
-let citySearchFormEl = document.getElementById("city-search-form");
-let citySearchResultsEl = document.getElementById("city-search-results");
+let searchFormEl = document.getElementById("city-search-form");
+let searchResultsEl = document.getElementById("city-search-results");
+let locationContainerEl = document.getElementById("location-container");
 
-citySearchFormEl.addEventListener("submit", (event) => {
+let locationList = [];
+
+searchFormEl.addEventListener("submit", (event) => {
   event.preventDefault();
   submitSearchForm(event.target);
 });
+
+searchResultsEl.addEventListener("click", (event) => {
+  let location = {
+    name: event.target.textContent,
+    lon: event.target.dataset.lon,
+    lat: event.target.dataset.lat,
+  };
+
+  if (isAlreadyInLocationList(location)) {
+    console.log("ALREADY EXISTS");
+    // TODO Add a modal to notify the user.
+  } else if (event.target.dataset.lon && event.target.dataset.lat) {
+    // Add this city to history list
+    locationList.push(location);
+    locationContainerEl.append(event.target);
+    // get weather info for this location
+    clearSearchResults();
+    updateLocalStorage();
+  }
+});
+
+function init() {
+  locationList = JSON.parse(localStorage.getItem("locationList"));
+
+  if (locationList) {
+    renderLocationListItems();
+  } else {
+    locationList = [];
+  }
+}
+
+init();
 
 async function submitSearchForm(form) {
   var searchResults = [];
@@ -18,27 +53,75 @@ async function submitSearchForm(form) {
   }
   searchResults = await getCoords(formData.citySearch);
   console.log(searchResults);
-  generateCitySearchResults(searchResults);
+  renderSearchResults(searchResults);
 }
 
-function generateCitySearchResults(searchResults) {
-  citySearchResultsEl
-    .querySelectorAll("*")
-    .forEach((element) => element.remove());
-  searchResults.forEach((result) => {
-    let li = document.createElement("p");
-    li.setAttribute("data-lon", result.lon);
-    li.setAttribute("data-lat", result.lat);
-    let span = document.createElement("span");
-    span.textContent = `${result.name}, ${result.state}`;
-    li.append(span);
-    let button = document.createElement("button");
-    button.textContent = "Select";
-    li.append(button);
-    citySearchResultsEl.append(li);
+function renderSearchResults(searchResults) {
+  clearSearchResults();
+  let cleanedResults = removeDuplicateSearchResults(searchResults);
+  cleanedResults.forEach((result) => {
+    let locationData = locationButtonObj(result);
+    searchResultsEl.append(locationButton(locationData));
   });
+}
+
+function renderLocationListItems() {
+  console.log(locationList);
+  locationList.forEach((location) => {
+    locationContainerEl.append(locationButton(location));
+  });
+}
+
+function locationButton(buttonData) {
+  console.log(buttonData);
+  let button = document.createElement("button");
+  button.setAttribute("data-lon", buttonData.lon);
+  button.setAttribute("data-lat", buttonData.lat);
+  button.textContent = `${buttonData.name}`;
+  return button;
 }
 
 function containsInvalidCharacters(search) {
   return /([0-9])+|,|\.|&/.test(search);
+}
+
+function removeDuplicateSearchResults(arr) {
+  let newArr = arr.filter(
+    (value, index, self) =>
+      index ===
+      self.findIndex(
+        (t) =>
+          t.name === value.name &&
+          t.state === value.state &&
+          t.country === value.country
+      )
+  );
+  return newArr;
+}
+
+function clearSearchResults() {
+  searchResultsEl.querySelectorAll("*").forEach((element) => element.remove());
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("locationList", JSON.stringify(locationList));
+}
+
+function locationButtonObj(data) {
+  let name = "";
+  if (data.name) name += data.name;
+  if (data.state) name += `, ${data.state}`;
+  if (data.country) name += `, ${data.country}`;
+
+  return {
+    name: name,
+    lon: data.lon,
+    lat: data.lat,
+  };
+}
+
+function isAlreadyInLocationList(location) {
+  return locationList.find((entry) => {
+    return entry.name === location.name;
+  });
 }
