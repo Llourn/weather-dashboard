8c74e08208d3071934e55feceb20e5f6a9512aef
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-02-19 16:15:28 -0400

    added todays weather to card render

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { fetchCoords, fetchForecast, fetchWeather } from "./fetchers.js";
 
+let dayjsObj = dayjs();
+
 let searchFormEl = document.getElementById("city-search-form");
 let searchResultsEl = document.getElementById("city-search-results");
 let locationContainerEl = document.getElementById("location-container");
@@ -81,7 +83,7 @@ async function getWeather(location) {
     location.dataset.lat,
     location.dataset.lon
   );
-  renderWeather(weatherData, forecastData);
+  renderWeather(location.textContent, weatherData, forecastData);
 }
 
 function renderSearchResults(searchResults) {
@@ -99,63 +101,86 @@ function renderLocationListItems() {
     let locationEl = document.createElement("div");
     locationEl.append(locationButton(location));
     let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("button", "is-danger");
     deleteBtn.dataset.locationIndex = index;
-    deleteBtn.textContent = "❌";
+    // deleteBtn.textContent = "X";
+    let icon = document.createElement("i");
+    icon.classList.add("fa-sharp", "fa-solid", "fa-xmark");
+    deleteBtn.append(icon);
+    // deleteBtn.innerHTML = `<i class="fa-sharp fa-solid fa-xmark"></i>`;
     locationEl.append(deleteBtn);
     locationContainerEl.append(locationEl);
   });
 }
 
-function renderWeather(weatherData, forecastData) {
+function renderWeather(locationName, weatherData, forecastData) {
+  weatherDisplayEl.append(buildWeatherCard(weatherData, locationName));
+
   forecastData.list.forEach((timeslot) => {
-    let day = timeslot.dt_txt.split(" ")[0].split("-");
-    let dayFormatted = `${day[1]}/${day[2]}/${day[0]}`;
     let time = timeslot.dt_txt.split(" ")[1];
 
     if (time.includes("12")) {
-      let weatherEl = document.createElement("div");
-      let dayEl = document.createElement("p");
-      dayEl.textContent = dayFormatted;
-      let weatherImgContainer = document.createElement("div");
-
-      timeslot.weather.forEach((element) => {
-        let weatherImgEl = document.createElement("img");
-        weatherImgEl.src = `https://openweathermap.org/img/wn/${element.icon}@2x.png`;
-        weatherImgContainer.append(weatherImgEl);
-      });
-      let tempEl = document.createElement("p");
-      tempEl.textContent = `Temp: ${Math.round(timeslot.main.temp)}`;
-      let feelsLikeEl = document.createElement("p");
-      feelsLikeEl.textContent = `Feels like: ${Math.round(
-        timeslot.main.feels_like
-      )}`;
-      let windEl = document.createElement("div");
-      windEl.textContent = `Wind: ${getCompassDirection(
-        timeslot.wind.deg
-      )} ${Math.round(timeslot.wind.speed * 3.6)} km/h`;
-      let humidityEl = document.createElement("p");
-      humidityEl.textContent = `Humidity: ${timeslot.main.humidity}%`;
-
-      weatherEl.append(
-        dayEl,
-        weatherImgContainer,
-        tempEl,
-        feelsLikeEl,
-        windEl,
-        humidityEl
-      );
-
-      weatherDisplayEl.append(weatherEl);
+      weatherDisplayEl.append(buildWeatherCard(timeslot));
     }
   });
 }
 
 function locationButton(buttonData) {
   let button = document.createElement("button");
+  button.classList.add("button");
   button.setAttribute("data-lon", buttonData.lon);
   button.setAttribute("data-lat", buttonData.lat);
   button.textContent = `${buttonData.name}`;
   return button;
+}
+
+function buildWeatherCard(timeslot, locationName = "") {
+  let title;
+  let classNames = ["column"];
+  if (locationName) {
+    classNames.push("todays-weather");
+    title = `${locationName} (${dayjsObj.format("dddd D MMM YYYY")})`;
+  } else {
+    classNames.push("five-day-weather");
+    let day = timeslot.dt_txt.split(" ")[0].split("-");
+    title = `${day[1]}/${day[2]}/${day[0]}`;
+  }
+
+  let weatherEl = document.createElement("div");
+  weatherEl.classList.add(...classNames);
+  let titleEl = document.createElement("p");
+  titleEl.textContent = title;
+  let weatherImgContainer = document.createElement("div");
+
+  timeslot.weather.forEach((element) => {
+    let weatherImgEl = document.createElement("img");
+    weatherImgEl.src = `https://openweathermap.org/img/wn/${element.icon}@2x.png`;
+    weatherImgEl.alt = element.description;
+    weatherImgContainer.append(weatherImgEl);
+  });
+  let tempEl = document.createElement("p");
+  tempEl.textContent = `Temp: ${Math.round(timeslot.main.temp)}`;
+  let feelsLikeEl = document.createElement("p");
+  feelsLikeEl.textContent = `Feels like: ${Math.round(
+    timeslot.main.feels_like
+  )}`;
+  let windEl = document.createElement("div");
+  windEl.textContent = `Wind: ${getCompassDirection(
+    timeslot.wind.deg
+  )} ${Math.round(timeslot.wind.speed * 3.6)} km/h`;
+  let humidityEl = document.createElement("p");
+  humidityEl.textContent = `Humidity: ${timeslot.main.humidity}%`;
+
+  weatherEl.append(
+    titleEl,
+    weatherImgContainer,
+    tempEl,
+    feelsLikeEl,
+    windEl,
+    humidityEl
+  );
+
+  return weatherEl;
 }
 
 function containsInvalidCharacters(search) {
